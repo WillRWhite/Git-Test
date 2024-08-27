5fd437586238458ab2ebae9f6ebf84343d02a11d
William White <will@wwhite.co.uk>
2024-08-27 16:18:42 +0100

    modified:   one.py

--- one.py
+++ one.py
@@ -1,6 +1,12 @@
 print("This is file one")
 print ("This is an update")
 print("This is another change")
+print("Okay another commit to test commit message")
+
+# So Commit message needs comment removing before commiting
+      
+      
+      
 
 
 
